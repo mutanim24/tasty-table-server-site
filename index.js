cec3c926d2d7ca09da62570a1b9dda0ca764baef
mutanim24 <mutanim24@gmail.com>
2023-05-04 00:31:55 +0600

    data updated

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 var cors = require('cors')
 const port = 4000;
 
 app.use(cors())
 
 const chefs = require('./data/chefs.json')
+const recipes = require('./data/recipes.json')
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -19,6 +20,10 @@ app.get('/chefs/:id', (req, res) => {
   const id =  req.params.id;
   const chefDetails = chefs.find(chef => chef.id == id);
   res.send(chefDetails)
+})
+
+app.get('/recipes', (req, res) => {
+  res.send(recipes)
 })
 
 app.listen(port, () => {
